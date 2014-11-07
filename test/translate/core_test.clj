2251db03b3640e55dac0d0606a34c46ebe8d63c5
J. David Escobar <davidescobar1976@gmail.com>
2014-11-07 01:09:02 -0800

    Added unit tests for 'extract-yaml-value' function.

--- test/translate/core_test.clj
+++ test/translate/core_test.clj
@@ -1,7 +1,17 @@
 (ns translate.core-test
   (:require [clojure.test :refer :all]
-            [translate.core :refer :all]))
+            [translate.core :refer :all]
+            [clojure.string :as string]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest extract-yaml-value-test
+  (testing "Lines in the format 'key: value'"
+    (let [ line "view: Vista" ]
+      (is (= (extract-yaml-value line) "Vista"))))
+
+  (testing "Lines in the format 'key: value # with a comment'"
+    (let [ line "time: tiempo # This is spanish." ]
+      (is (= (extract-yaml-value line) "tiempo"))))
+
+  (testing "Lines in the format 'key:' and comments-only lines '# Just a comment on this line.'"
+    (let [ lines [ "key:", "    sub-categories:      ", "# Just a comment on this line." ] ]
+      (is (= true (every? #(string/blank? (extract-yaml-value %)) lines))))))
